260aed18067e70fc23d6b21d0a57d22139608ee5
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-08-08 05:33:19 -0700

    done 002_condition

--- 001_syntax/002_condition.js
+++ 001_syntax/002_condition.js
@@ -6,7 +6,14 @@
  *
  */
 function printMessageByAge(age) {
+  if (age >= 20) {
+    console.log(`You are already adult.`)
+  } else {
+    console.log(`You are still a child.`)
+  }
+  // age >= 20 ? console.log(`You are already adult.`) : console.log(`You are still a child.`)
 }
+console.log(printMessageByAge(21));
 
 /**
  *  2.2 引数から与えられた時間が
@@ -17,7 +24,17 @@ function printMessageByAge(age) {
  *
  */
 function greeding(hour) {
+  if (hour >= 4 && hour < 12) {
+    alert(`Good Morning !`);
+  } else if (hour >= 12 && hour < 17) {
+    alert(`Hello`);
+  } else if (hour >= 17) {
+    alert(`Good Night`);
+  } else {
+    alert(`Good Night`);
+  }
 }
+console.log(greeding(12));
 
 /**
  *  2.3 引数から与えられた数字に対応する曜日を返却するメソッドを実装してください
@@ -33,7 +50,26 @@ function greeding(hour) {
  */
 
 function getDay(day) {
+  switch (new Date().getDay()) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      break;
+  }
 }
+console.log(getDay(0))
 
 module.exports = {
   printMessageByAge,
